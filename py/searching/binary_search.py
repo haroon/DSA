39ab9a9ae348a8abf3e2f7407479c6b478dc79c5
haroon <haroon.shafiq@gmail.com>
2017-04-23 20:07:00 +1000

    Removed unused code.

--- py/searching/binary_search.py
+++ py/searching/binary_search.py
@@ -1,6 +1,3 @@
-lst1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-lst2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
 def binary_search(haystack, needle):
     """Implementation of binary search algorithm.
     
