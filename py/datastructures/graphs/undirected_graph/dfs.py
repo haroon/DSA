04f08c1d5c727c1bf9561dd9ac73c4159432e438
Haroon Shafiq <haroon.shafiq@iress.com.au>
2017-05-04 15:25:32 +1000

    Added deapth first search for undirected graph and a test method.

---
+++ py/datastructures/graphs/undirected_graph/dfs.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from random import seed, randint
+
+from .undirected_graph import undirected_graph
+
+class DFSPath(object):
+    def __init__(self, g, v):
+        self.graph = g
+        self.vertex = v
+        self.edge_from = [None] * self.graph.V
+        self.visited = [False] * self.graph.V
+        self._do_dfs(self.vertex)
+
+    def _do_dfs(self, vertex):
+        self.visited[vertex] = True
+        adjacent_vertices = self.graph.adj(vertex)
+        for v in adjacent_vertices:
+            if not self.visited[v]:
+                self._do_dfs(v)
+                self.edge_from[v] = vertex
+
+    def is_connected(self, s):
+        return self.visited[s]
+
+    def path_to(self, v):
+        if not self.is_connected(v):
+            return None
+        path = [v]
+        while v != self.vertex:
+            v = self.edge_from[v]
+            path.append(v)
+        return path
+
+MAX_VERTICES = 20
+
+
+def test_dfs_path():
+    seed(datetime.now().timestamp())
+    ug = undirected_graph(MAX_VERTICES)
+
+    test_vertices = []
+
+    for i in range(MAX_VERTICES):
+        v1 = randint(0, MAX_VERTICES - 1)
+        v2 = randint(0, MAX_VERTICES - 1)
+        test_vertices.append((v1, v2))
+        ug.add_edge(v1, v2)
+
+    print(test_vertices)
+
+    v = randint(0, MAX_VERTICES - 1)
+    print(v)
+    paths = DFSPath(ug, v)
+    v = randint(0, MAX_VERTICES - 1)
+    print(v)
+    path = paths.path_to(v)
+    print(path)
--- py/main.py
+++ py/main.py
@@ -1,5 +1,6 @@
 """Main file for data structure and algorithm scripts."""
 from datastructures.graphs.undirected_graph.undirected_graph import undirected_graph
+from datastructures.graphs.undirected_graph.dfs import DFSPath, test_dfs_path
 from searching.binary_search import binary_search
 from sorting.bubble_sort import naive_bubble_sort, optimized_bubble_sort
 from util.util import get_random_int_list, make_comparator
